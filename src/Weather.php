3ff7df9af00de7e8a0e6d32821f2413848eb6a49
ttheitao <452036812@qq.com>
2019-03-02 08:34:55 +0000

    去掉Weather类构造函数 string 类型约束

--- src/Weather.php
+++ src/Weather.php
@@ -26,7 +26,7 @@ class Weather
      *
      * @param string $key
      */
-    public function __construct(string $key)
+    public function __construct($key)
     {
         $this->key = $key;
     }
